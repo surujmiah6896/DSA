65a145289c8c434f63efa72eea4270f2d001ce25
surujmiah6896 <surujmiah6896@gmail.com>
2025-06-25 12:04:59 +0600

    lifo or stack

---
+++ stack.py
@@ -0,0 +1,8 @@
+# Stack using list with LIFO
+stack = []
+stack.append(1)
+stack.append(2)
+stack.append(3)
+print("final stack:", stack)
+print("Popped:", stack.append(4))  # LIFO
+print("Stack:", stack)
