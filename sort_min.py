38af6ea096a847b9a03d82dd86c0bd56f5e2cf20
surujmiah6896 <surujmiah6896@gmail.com>
2025-06-25 13:24:12 +0600

    find min from list

--- sort_min.py
+++ sort_min.py
@@ -2,7 +2,7 @@ my_array = [64, 34, 25, 12, 22, 11, 90, 5]
 for i in range(len(my_array)):
     max_index = i
     for j in range(len(my_array) - 1):
-        if my_array[j] > my_array[max_index]:
+        if my_array[j] < my_array[max_index]:
             max_index = j
 
-print("max value is :", my_array[max_index])
+print("min value is :", my_array[max_index])
