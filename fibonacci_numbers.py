ab992fd6b63c802f7090ef673b51720df36619ad
surujmiah6896 <surujmiah6896@gmail.com>
2025-06-25 10:29:45 +0600

    init DSA

---
+++ fibonacci_numbers.py
@@ -0,0 +1,11 @@
+# write with loop
+# num1 = 0
+# num2 = 1
+
+# print(num1)
+# print(num2)
+# for fibo in range(18):
+#     newfibo = num2 + num1
+#     print(newfibo)
+#     num1 = num2
+#     num2 = newfibo
