2c7e8d3b5ca21f5ea5b8fd2b5c6af797bcb0b1ee
surujmiah6896 <surujmiah6896@gmail.com>
2025-06-25 23:50:38 +0600

    solve with hash for more clean

--- two_sum.py
+++ two_sum.py
@@ -1,17 +1,44 @@
 my_array = [5, 2, 11, 7, 15]
 
 # # brutef
-n = len(my_array)
+# n = len(my_array)
+# target = 9
+# answer = []
+# for i in range(n):
+#     first_num = my_array[i]
+#     for j in range(i + 1, n):
+#         second_num = my_array[j]
+#         sum = first_num + second_num
+#         if sum == target:
+#             answer.append(first_num)
+#             answer.append(second_num)
+
+
+# print("Answer is:", answer)
+
+
+class Solution:
+    def twoSum(self, nums: list[int], target: int) -> list[int]:
+        hash_map = {}  # value -> index
+
+        for i, num in enumerate(nums):
+            complement = target - num
+            if complement in hash_map:
+                return [hash_map[complement], i]
+            hash_map[num] = i
+
+        return []
+
+
+solution = Solution()
+nums = [5, 2, 11, 7, 15]
 target = 9
-answer = []
-for i in range(n):
-    first_num = my_array[i]
-    for j in range(i + 1, n):
-        second_num = my_array[j]
-        sum = first_num + second_num
-        if sum == target:
-            answer.append(first_num)
-            answer.append(second_num)
+result = solution.twoSum(nums, target)
+
+# print(result)
 
 
-print("Answer is:", answer)
+my_hash = {}
+my_hash[3] = 2
+
+print(my_hash)
