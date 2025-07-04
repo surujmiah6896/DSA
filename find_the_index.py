8aa8967547ee8fa675991e930b58ae94723be7d3
surujmiah6896 <surujmiah6896@gmail.com>
2025-07-04 23:47:43 +0600

    find first element and return index

---
+++ find_the_index.py
@@ -0,0 +1,14 @@
+class Solution:
+    def strStr(self, haystack: str, needle: str) -> int:
+        # check needle
+        if not needle:
+            return 0
+        # loop for find index
+        for i in range(len(haystack) - len(needle) + 1):
+            if haystack[i:i+len(needle)] == needle:
+                # return index
+                return i
+
+        return -1
+
+
