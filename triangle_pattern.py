eb9901755b84787258fed4b80d324cc0f24c0545
surujmiah6896 <surujmiah6896@gmail.com>
2025-10-02 19:57:30 +0600

    butterfly border

--- triangle_pattern.py
+++ triangle_pattern.py
@@ -84,22 +84,40 @@ n = 5
 #     print()
 
 # diamond border pattern
-for i in range(n):
-    for j in range(n - i - 1):
-        print(" ", end="")
-    for k in range(2 * i + 1):
-        if k == 0 or k == 2 * i:
-            print("*", end="")
-        else:
-            print(" ", end="")
-    print()
-for i in range(n - 1, 0, -1):
-    for j in range(n - i):
-        print(" ", end="")
-    for k in range(2 * i - 1):
-        if k == 0 or k == 2 * i - 2:
-            print("*", end="")
-        else:
-            print(" ", end="")
-    print()   
+# for i in range(n):
+#     for j in range(n - i - 1):
+#         print(" ", end="")
+#     for k in range(2 * i + 1):
+#         if k == 0 or k == 2 * i:
+#             print("*", end="")
+#         else:
+#             print(" ", end="")
+#     print()
+# for i in range(n - 1, 0, -1):
+#     for j in range(n - i):
+#         print(" ", end="")
+#     for k in range(2 * i - 1):
+#         if k == 0 or k == 2 * i - 2:
+#             print("*", end="")
+#         else:
+#             print(" ", end="")
+#     print()   
  
+
+# butterfly pattern
+for i in range(n):
+    for j in range(i + 1):
+        print("*", end="")
+    for k in range(2 * (n - i - 1)):
+        print(" ", end="")
+    for l in range(i + 1):
+        print("*", end="")
+    print()
+for i in range(n - 1, -1, -1):
+    for j in range(i + 1):
+        print("*", end="")
+    for k in range(2 * (n - i - 1)):
+        print(" ", end="")
+    for l in range(i + 1):
+        print("*", end="")
+    print() 
