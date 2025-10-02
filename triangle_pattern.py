2d41516ba1538633bfd9adf91b415a3493fdee53
surujmiah6896 <surujmiah6896@gmail.com>
2025-10-02 18:23:29 +0600

    floyd's truangle ptattern

--- triangle_pattern.py
+++ triangle_pattern.py
@@ -1,13 +1,20 @@
 n = 10
 
+# for i in range(n):
+#     for j in range(i + 1):
+#         print("*", end="")
+#     print()
+
+# # reverse triangle
+# for i in range(n, 0, -1):
+#     for j in range(i):
+#         print("* ", end="")
+#     print()
+
+# floyd's triangle
+num = 1
 for i in range(n):
     for j in range(i + 1):
-        print("*", end="")
+        print(num, end=" ")
+        num += 1
     print()
-
-# reverse triangle
-for i in range(n, 0, -1):
-    for j in range(i):
-        print("* ", end="")
-    print()
-
