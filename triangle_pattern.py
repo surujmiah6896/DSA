734a1631a37b94c8ee7f5478183880cf09f589ff
surujmiah6896 <surujmiah6896@gmail.com>
2025-10-02 19:51:59 +0600

    diamond border

--- triangle_pattern.py
+++ triangle_pattern.py
@@ -44,27 +44,62 @@ n = 5
 #     print()
 
 # pyramid number pattern
+# for i in range(n):
+#     for j in range(n - i - 1):
+#         print(" ", end="")
+#     for k in range(2 * i + 1):
+#         print(i + 1, end="")
+#     print()
+
+# # pyramid number reverse pattern
+# for i in range(n):
+#     for j in range(n - i - 1):
+#         print(" ", end="")
+#     for k in range(2 * i + 1):
+#         print(n - i, end="")
+#     print()
+
+# # pyramid number updown pattern
+# for i in range(n):
+#     for j in range(n - i - 1):
+#         print(" ", end="")
+#     for k in range(i + 1):
+#         print(k + 1, end="")
+#     for l in range(i, 0, -1):
+#         print(l, end="")
+#     print() 
+
+# diamond pattern
+# for i in range(n):
+#     for j in range(n - i - 1):
+#         print(" ", end="")
+#     for k in range(2 * i + 1):
+#         print("*", end="")
+#     print()
+# for i in range(n - 1, 0, -1):
+#     for j in range(n - i):
+#         print(" ", end="")
+#     for k in range(2 * i - 1):
+#         print("*", end="")
+#     print()
+
+# diamond border pattern
 for i in range(n):
     for j in range(n - i - 1):
         print(" ", end="")
     for k in range(2 * i + 1):
-        print(i + 1, end="")
+        if k == 0 or k == 2 * i:
+            print("*", end="")
+        else:
+            print(" ", end="")
     print()
-
-# pyramid number reverse pattern
-for i in range(n):
-    for j in range(n - i - 1):
+for i in range(n - 1, 0, -1):
+    for j in range(n - i):
         print(" ", end="")
-    for k in range(2 * i + 1):
-        print(n - i, end="")
-    print()
-
-# pyramid number updown pattern
-for i in range(n):
-    for j in range(n - i - 1):
-        print(" ", end="")
-    for k in range(i + 1):
-        print(k + 1, end="")
-    for l in range(i, 0, -1):
-        print(l, end="")
-    print() 
+    for k in range(2 * i - 1):
+        if k == 0 or k == 2 * i - 2:
+            print("*", end="")
+        else:
+            print(" ", end="")
+    print()   
+ 
