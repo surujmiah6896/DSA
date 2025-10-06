7b3733f9ddb12f7902e4a8569e1015cb8c675f9c
surujmiah6896 <surujmiah6896@gmail.com>
2025-10-06 14:31:09 +0600

    reverse array

---
+++ array/reverseArray.py
@@ -0,0 +1,20 @@
+arr = [1, 3, 4, 5, 6, 8, 90, 43]
+
+#this is 2 point approach
+def reverseArray(array):
+    arrLength = len(array)
+    start = 0
+    end = arrLength - 1
+
+    while start < end:
+        print('start', start)
+        print('end', end)
+        array[start], array[end] = array[end], array[start]
+        start += 1
+        end -= 1
+    
+    print(array)
+    return array
+
+
+reverseArray(arr)
