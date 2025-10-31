82bac1d44a60949850bd30e26ddafac33b81f065
surujmiah6896 <surujmiah6896@gmail.com>
2025-10-31 17:51:28 +0600

    create new array from nested array

---
+++ js/create_new_array_from_nested.js
@@ -0,0 +1,37 @@
+var arr = [
+  1,
+  2,
+  3,
+  [4, 6, 7],
+  [
+    [8, 9, 10],
+    [11, 12, 13],
+  ],
+];
+
+function createNewArray(arr) {
+  if (typeof arr != "object" || arr == "" || arr == null) {
+    console.log("please enter valid array");
+    return false;
+  }
+
+  const newArray = [];
+
+  function flatten(input) {
+    var len = input.length;
+    for (i = 0; i < len; i++) {
+      const item = input[i];
+      if (Number.isInteger(item)) {
+        newArray.push(item);
+      } else if (Array.isArray(item)) {
+        flatten(item);
+      }
+    }
+  }
+
+  flatten(arr);
+
+  return newArray;
+}
+
+console.log(createNewArray(arr));
