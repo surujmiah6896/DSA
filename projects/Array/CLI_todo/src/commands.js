d4f64bb4dc8277375b91e0c5faae16bb715634bf
surujmiah6896 <surujmiah6896@gmail.com>
2025-11-02 14:18:15 +0600

    ceate commands const

---
+++ projects/Array/CLI_todo/src/commands.js
@@ -0,0 +1,17 @@
+const ADD = 'add';
+const LIST = 'list';
+const COMPLETE = 'complete';
+const UPDATE = 'update';
+const DELETE = 'delete';
+const NEXT = 'next';
+const FIND = 'find';
+
+module.exports = {
+  ADD,
+  LIST,
+  COMPLETE,
+  UPDATE,
+  DELETE,
+  NEXT,
+  FIND,
+};
