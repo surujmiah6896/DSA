461833166c47a48159df075fb776b75b4034339b
surujmiah6896 <surujmiah6896@gmail.com>
2025-11-02 14:24:15 +0600

    ceate utils file

---
+++ projects/Array/CLI_todo/src/utils.js
@@ -0,0 +1,25 @@
+const fs = require('fs');
+const path = require('path');
+
+
+module.exports.saveFile = (fileName, data) => {
+    try{
+        fs.writeFileSync(fileName, JSON.stringify(data, null, 2), 'utf-8');
+    }catch(err){
+        console.error("Error writing file:", err);
+        throw new Error('Error saving file: ' + err.message);
+    }
+};
+
+module.exports.readFile = (fileName) => {
+    try{
+        if(!fs.existsSync(fileName)){
+            return [];
+        }
+        const data = fs.readFileSync(fileName, 'utf-8');    
+        return JSON.parse(data);
+    }catch(err){
+        console.error("Error reading file:", err);
+        throw new Error('Error reading file: ' + err.message);
+    }
+};
