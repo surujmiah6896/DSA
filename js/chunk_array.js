239a9ecd8ad89003d049c04ce7ef5feaf4153155
surujmiah6896 <surujmiah6896@gmail.com>
2025-11-07 00:08:56 +0600

    create chunk of array

--- js/chunk_array.js
+++ js/chunk_array.js
@@ -29,3 +29,15 @@ const r = chunk(arr, size);
 console.log('chunk array:', r);
 
 
+// update item
+var chunk = (arr, size) => {
+  const result = [];
+
+  for (let i = 0; i < arr.length; i+=size) {
+        result.push(arr.slice(i,i+size));
+  }
+  return result;
+};
+
+const r1 = chunk(arr, size);
+console.log("chunk1 array:", r1);
