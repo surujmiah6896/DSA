97790d702efb9193976f8e95a8b2d4be3979fd7b
surujmiah6896 <surujmiah6896@gmail.com>
2025-11-25 00:19:51 +0600

    compress docoment sudocode

--- js/array/compress.js
+++ js/array/compress.js
@@ -20,13 +20,28 @@ var compress = function (chars) {
     writeIndex++;
 
     if (count > 1) {
-      const countStr = count.toString();
-      for (let digit of countStr) {
-        chars[writeIndex] = digit;
-        writeIndex++;
-      }
+        const countStr = count.toString();
+        for (let digit of countStr) {
+            chars[writeIndex] = digit;
+            writeIndex++;
+        }
     }
   }
 
   return writeIndex;
 };
+
+//# sudocode
+//step 1: init read and write index with 0
+//step 2: while start read to chars.length
+//step 3: get first or current chars
+//step 4: current item matching with all elements via loops
+//step 5: set currentChar with current position
+//step 6: increment writeIndex
+//step 7: if count > 1
+//step 5: then set count with this char
+//step 6: return writeIndex
+
+
+const chars = ["a", "a", "b", "b", "b", "c"];
+console.log(compress(chars));
