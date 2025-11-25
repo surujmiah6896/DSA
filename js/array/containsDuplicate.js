7151b880c42f10e8e9a3ad79d94ac6f0044ea136
surujmiah6896 <surujmiah6896@gmail.com>
2025-11-25 23:56:46 +0600

    containsNearbyDuplicate with map

--- js/array/containsDuplicate.js
+++ js/array/containsDuplicate.js
@@ -16,6 +16,24 @@ var containsNearbyDuplicate = function(nums, k) {
 }
 
 
-const nums = [1, 2, 3, 1], k = 3;
+// const nums = [1, 2, 3, 1], k = 3;
 
-console.log(containsNearbyDuplicate(nums, k));
+// console.log(containsNearbyDuplicate(nums, k));
+
+
+
+var containsNearbyDuplicateWithMap = function (nums, k) {
+  const map = new Map();
+
+  for (let i = 0; i < nums.length; i++) {
+    const num = nums[i];
+
+    if (map.has(num) && i - map.get(num) <= k) {
+      return true;
+    }
+
+    map.set(num, i);
+  }
+
+  return false;
+};
