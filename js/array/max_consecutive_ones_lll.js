0c12070461fd55f822065abb2ab46ed2a7f0c3c4
surujmiah6896 <surujmiah6896@gmail.com>
2025-11-27 14:29:11 +0600

    max consecutive ones_lll

---
+++ js/array/max_consecutive_ones_lll.js
@@ -0,0 +1,39 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+var longestOnes = function (nums, k) {
+    let left = 0;
+    let right = 0;
+    let maxLength = 0;
+    let zeroCount = 0;
+
+    while(right < nums.length){
+        if(nums[right] === 0){
+            zeroCount++;
+        }
+
+        while(zeroCount > k){
+            if(nums[left] === 0){
+                zeroCount--;
+            }
+            left++;
+        }
+
+        maxLength = Math.max(maxLength, right-left + 1);
+        right++;
+    }
+    return maxLength;
+};
+
+//step 1: init left, right, maxLength and zeroCount;
+//step 2: check item is 0 and count 0
+//step 3: remove item if 0 max k from left and left increment
+//step 4: set max number
+//step 5: return max
+
+
+const nums = [1,1,1,0,0,0,1,1,1,1,0], k = 2;
+
+console.log(longestOnes(nums, k));
