df5bbf87af4c2fe771f848daf37f1b8dddd8b7fa
surujmiah6896 <surujmiah6896@gmail.com>
2025-11-27 16:50:36 +0600

    find privot index

---
+++ js/prefix/find_pivot_index.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var pivotIndex = function (nums) {
+    const totalSum = nums.reduce((acc, curr) => acc + curr, 0);
+    let leftSum = 0;
+
+    for(let i = 0; i < nums.length; i++){
+        if(leftSum === totalSum - leftSum - nums[i]){
+            return i;
+        }
+        leftSum += nums[i];
+    }
+
+    return -1;
+};
+
+
+const nums = [1, 7, 3, 6, 5, 6];
+
+console.log(pivotIndex(nums));
