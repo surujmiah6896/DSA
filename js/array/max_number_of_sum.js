644075ecc393156dd195c24283e8f13b5d37b9bf
surujmiah6896 <surujmiah6896@gmail.com>
2025-11-27 12:06:06 +0600

    max number of sum

--- js/array/max_number_of_sum.js
+++ js/array/max_number_of_sum.js
@@ -37,10 +37,46 @@ var maxOperations = function (nums, k) {
 //step 3: break loop when left big from right
 //step 4: check sum and count operation
 //Note if match then shift both other wise shift left
+//Time Complexity: O(n log n)
+//Space Complexity: O(1) or O(n) depending on sort implementation
+
+// const nums = [1, 2, 3, 4],
+//   k = 5;
 
 
-const nums = [1, 2, 3, 4],
-  k = 5;
+//   console.log(maxOperations(nums, k));
 
 
-  console.log(maxOperations(nums, k));
+  //Hash Map for optimal.is apply when we can not use sorted
+
+  var maxOperationsWithHash = function (nums, k) {
+    const freq = new Map();
+    console.log(freq);
+    let operations = 0;
+
+    for(const num of nums){
+        const complement = k - num;
+
+        if(freq.has(complement) && freq.get(complement) > 0){
+            operations++;
+            freq.set(complement, freq.get(complement) - 1);
+        }else{
+            freq.set(num, (freq.get(num) || 0) + 1);
+        }
+    }
+
+    return operations;
+  };
+
+
+  //step 1: create map for hash
+  //step 2: create complement from k
+  //step 3: check and count if exist and big of 0
+  //step 4: set hash with number
+  //step 5: return operations
+
+
+  const nums = [1, 2, 3, 4],
+    k = 5;
+
+  console.log(maxOperationsWithHash(nums, k));
